ac83fe6ed02d0d4d7d9c56a89ebcec6e92c027ca
João Rodrigues <geral@joaorodrigues-multimedia.pt>
2026-07-24 14:33:59 +0100

    refatoracao: consolidar providers da aplicacao

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,14 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
 
 /**
- * Retorna os serviços da aplicação
+ * Define os providers específicos da aplicação.
  *
- * @since 1.0
+ * Os nomes das classes permanecem em inglês por seguirem as convenções dos
+ * providers do Laravel.
  *
- * @version 1.0
+ * @since 1.0.0
+ *
+ * @version 2.0.0
  */
 return [
     AppServiceProvider::class,
